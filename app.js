2f8761ca29d6485795b215fc9759a32e94772af5
Utkrisht <utkrishtkumar8191@gmail.com>
2022-09-08 18:43:41 +0530

    Added a comment in app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-//
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
