29c2be1eab14fc1543cd8266c13607268b422cae
Utkrisht <utkrishtkumar8191@gmail.com>
2022-09-08 20:37:52 +0530

    Changing start of package.json

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.log('DB connection successful!');
   });
 
-const port = process.env.PORT2 || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
